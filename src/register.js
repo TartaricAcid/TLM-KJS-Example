b9d9c394389830737b1007c174fb619ce12481f0
tartaric_acid <baka943@qq.com>
2025-06-12 18:13:52 +0800

    Update register.js

--- src/register.js
+++ src/register.js
@@ -21,7 +21,6 @@ MaidRegister.TIPS.tips(overlay => {
         (stack, maid, player) => stack.is("minecraft:apple") && stack.getCount() > 2);
 });
 
-let MeleeAttack = Java.loadClass("net.minecraft.world.entity.ai.behavior.MeleeAttack");
 // 注册全新的工作模式
 // 这块较为复杂，需要掌握原版的 Brain 机制才能写出可用的内容
 MaidRegister.TASK
@@ -31,5 +30,18 @@ MaidRegister.TASK
     .enable(maid => maid.favorabilityManager.getLevel() >= 2)
     // 任务的启用提示文本（可选）
     .addEnableConditionDesc("need_level_2", maid => maid.favorabilityManager.getLevel() >= 2)
-    // 添加 Brain（可选）
-    .addBrain(5, MeleeAttack.create(20));
+    // 任务的攻击目标（可选）
+    .canAttack((maid, target) => target.type === "minecraft:cat")
+    // 攻击武器的判断
+    .isWeapon((maid, stack) => stack.is("minecraft:diamond_sword"))
+    // 添加 Brain（可选）。一般工作模式的优先级设置为 5 就行，Brain 可以使用 MaidBrainsTool 进行书写，这个方法提供了很多工具
+    // 女仆开始寻找周围可攻击的目标
+    .addBrain(5, (task, maid) => MaidBrainsTool.startAttacking(task))
+    // 女仆会在攻击目标无效，或者手上没有武器时时停止攻击
+    .addBrain(5, (task, maid) => MaidBrainsTool.stopAttackingIfTargetInvalid(task, maid))
+    // 当敌对生物超出近战范围，女仆会尝试走向敌人
+    .addBrain(5, (task, maid) => MaidBrainsTool.setWalkTargetFromAttackTargetIfTargetOutOfReach(0.6))
+    // 当在近战范围内，女仆会尝试使用近战武器攻击目标
+    .addBrain(5, (task, maid) => MaidBrainsTool.meleeAttack(20))
+    // 当女仆手上有盾牌时且受到伤害时，女仆会尝试使用盾牌格挡
+    .addBrain(5, (task, maid) => MaidBrainsTool.maidUseShieldTask());
