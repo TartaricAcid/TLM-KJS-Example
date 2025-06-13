82535e7e238a36ab26bb1e1a0237fe27a1ed6c93
tartaric_acid <baka943@qq.com>
2025-06-13 16:36:26 +0800

    Update register.js

--- src/register.js
+++ src/register.js
@@ -22,26 +22,89 @@ MaidRegister.TIPS.tips(overlay => {
 });
 
 // 注册全新的工作模式
-// 这块较为复杂，需要掌握原版的 Brain 机制才能写出可用的内容
+// 这块即可完全自定义自己的 Brain，也提供了预设模板略加修改
 MaidRegister.TASK
     // 设定工作模式的 ID 和图标
-    .attackTask("test:attack_cat", "minecraft:apple")
-    // 任务的启用条件（可选）
+    // meleeTask 就是预设的近战攻击任务，里面自带了近战相关的 AI，所以一般无需添加额外的 Brain
+    .meleeTask("test:attack_cat", "minecraft:apple")
+    // 任务的启用条件（可选，默认永远开启）
     .enable(maid => maid.favorabilityManager.getLevel() >= 2)
-    // 任务的启用提示文本（可选）
+    // 任务的启用提示文本（可选，默认无任何提示）
     .addEnableConditionDesc("need_level_2", maid => maid.favorabilityManager.getLevel() >= 2)
-    // 任务的攻击目标（可选）
-    .canAttack((maid, target) => target.type === "minecraft:cat")
-    // 攻击武器的判断
-    .isWeapon((maid, stack) => stack.is("minecraft:diamond_sword"))
-    // 添加 Brain（可选）。一般工作模式的优先级设置为 5 就行，Brain 可以使用 MaidBrainsTool 进行书写，这个方法提供了很多工具
-    // 女仆开始寻找周围可攻击的目标
-    .addBrain(5, (task, maid) => MaidBrainsTool.startAttacking(task))
-    // 女仆会在攻击目标无效，或者手上没有武器时时停止攻击
-    .addBrain(5, (task, maid) => MaidBrainsTool.stopAttackingIfTargetInvalid(task, maid))
-    // 当敌对生物超出近战范围，女仆会尝试走向敌人
-    .addBrain(5, (task, maid) => MaidBrainsTool.setWalkTargetFromAttackTargetIfTargetOutOfReach(0.6))
-    // 当在近战范围内，女仆会尝试使用近战武器攻击目标
-    .addBrain(5, (task, maid) => MaidBrainsTool.meleeAttack(20))
-    // 当女仆手上有盾牌时且受到伤害时，女仆会尝试使用盾牌格挡
-    .addBrain(5, (task, maid) => MaidBrainsTool.maidUseShieldTask());
+    // 任务的攻击目标（可选，默认为所有敌对生物）
+    .canAttack((maid, target) => target.type === "minecraft:cow")
+    // 攻击武器的判断（必填，否则女仆不执行任何攻击逻辑）
+    .isWeapon((maid, stack) => stack.is("minecraft:wooden_sword"))
+    // 拥有额外攻击，也就是在武器造成伤害之后还有额外的逻辑，（可选，默认为 false）
+    .hasExtraAttack((maid, target) => true)
+    // 执行额外攻击逻辑（可选，需要上一个返回 true 这块才会执行，默认为空）
+    .doExtraAttack(
+        /**
+         * @param {Internal.EntityMaid} maid - 女仆实体
+         * @param {Internal.Entity} target - 攻击对象
+         */
+        (maid, target) => {
+            // 给目标添加一个向上的运动
+            target.addMotion(0, 1, 0)
+            // 延迟半秒，在目标位置创建一个爆炸
+            target.level.server.scheduleInTicks(10, event => {
+                let pos = target.position()
+                target.level.createExplosion(pos.x, pos.y, pos.z)
+                    .strength(1).exploder(maid).explode();
+            })
+            // 返回 true 表示攻击成功，false 则表示攻击失败
+            return true;
+        })
+
+// 远程攻击任务
+MaidRegister.TASK
+    .rangedAttackTask("test:tnt_attack", "minecraft:tnt")
+    // 攻击武器的判断（必填，否则女仆不执行任何攻击逻辑）
+    .isWeapon((maid, stack) => {
+        // 武器必须是可以 use 的物品
+        return stack.is("minecraft:bow") &&
+            // 并且女仆的背包里有 TNT
+            MaidItemsUtil.isStackIn(maid, item => item.is("minecraft:tnt"));
+    })
+    // 任务的攻击目标（可选，默认为所有敌对生物）
+    .canAttack((maid, target) => target.type === "minecraft:cow")
+    // 生物搜索半径（可选，默认为工作范围）
+    .searchRadius(48)
+    // 弹药距离（可选，默认为 16）
+    .projectileRange(32)
+    // 攻击蓄力时间（可选，默认为 20 tick）
+    .chargeDurationTick(10)
+    // 执行远程攻击逻辑，必填项，否则啥也不干
+    .performRangedAttack(
+        /**
+         * @param {Internal.EntityMaid} maid - 女仆实体
+         * @param {Internal.LivingEntity} target - 攻击对象
+         * @param {number} distanceFactor - 距离因子，0-1 之间，表示目标距离女仆的远近，可与直接用于弹射物射速
+         */
+        (maid, target, distanceFactor) => {
+            // 计算起点和目标点
+            let start = maid.position();
+            let end = target.position();
+
+            // 创建 TNT 实体
+            let tnt = maid.level.createEntity("minecraft:tnt");
+            // 稍微高一点防止卡地面
+            tnt.setPosition(start.x, start.y + 1.0, start.z);
+            // 设置爆炸倒计时（单位：tick，默认 80）
+            tnt.fuse = 10;
+
+            // 计算速度向量
+            let dx = end.x - start.x;
+            // 适当调整高度
+            let dy = (end.y + 0.5) - (start.y + 1.0);
+            let dz = end.z - start.z;
+            let dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
+            let speed = distanceFactor;
+            tnt.setMotion(dx / dist * speed, dy / dist * speed + 0.2, dz / dist * speed);
+
+            // 生成 TNT
+            tnt.spawn();
+
+            // 消耗一个 TNT 物品
+            MaidItemsUtil.getStack(maid, item => item.is("minecraft:tnt")).shrink(1);
+        });
